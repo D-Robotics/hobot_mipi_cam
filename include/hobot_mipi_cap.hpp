43cf29e178a7f5b7a9d8c36d8b478855ad5d6295
GuangYuanNing <guangyuan.ning@horizon.ai>
2026-05-15 09:53:22 +0000

    rebuild create gdc bin for X5,S100,S600

--- include/hobot_mipi_cap.hpp
+++ include/hobot_mipi_cap.hpp
@@ -17,11 +17,30 @@
 
 #include <vector>
 #include <string>
+#if defined(PLATFORM_S100) || defined(PLATFORM_S600)
+#include "hb_gdc_cfg.h"
+#include "gdc_cfg.h"
+#else
+#include "gdc_cfg.h"
+#include "gdc_bin_cfg.h"
+#endif  
 #include "hobot_mipi_comm.hpp"
 #include "sensor_msgs/msg/camera_info.hpp"
 #include "hobot_mipi_calibration.hpp"
+#include "opencv2/opencv.hpp"
 
 namespace mipi_cam {
+
+typedef struct gdc_binbuf_s {
+  hb_mem_common_buf_t *bin_buf = nullptr;
+  uint64_t bin_buf_size;
+  ~gdc_binbuf_s() {
+    if (bin_buf != NULL) {
+      hb_mem_free_buf(bin_buf->fd);
+      bin_buf = NULL;
+    }
+  }
+}GdcBinBuf_ST;
 
 class HobotMipiCap {
  public:
@@ -71,7 +90,51 @@ class HobotMipiCap {
   virtual int setCamInfo(std::vector<sensor_msgs::msg::CameraInfo> info) {
     return -1;
   }
+#if 1
+ protected:
+  MIPI_CAP_INFO_ST cap_info_;
+  std::vector<std::shared_ptr<BuffQueueManage>> v_buff_que_manger_;
+  std::shared_ptr<BuffQueueManage> combine_buff_que_manger_;
+  std::vector<std::shared_ptr<FrameQueue>> v_frame_que_;
+  std::vector<std::shared_ptr<BuffQueueManage>> v_sub_buff_que_manger_;
+  std::shared_ptr<BuffQueueManage> sub_combine_buff_que_manger_;
+  std::vector<std::shared_ptr<FrameQueue>> v_sub_frame_que_;
 
+  void free_gdc_cfg(void *buf_ptr);
+  int32_t gen_gdc_cfg(param_t *param, window_t *wnds, uint32_t wnd_num, void **cfg_buf, uint64_t *cfg_size);
+  virtual void sync_task();
+  virtual void sub_sync_task();
+  virtual bool isSynced(const std::vector<std::shared_ptr<VideoBuffer>> &frames, long long tolerance);
+
+  std::shared_ptr<GdcBinBuf_ST> get_gdc_bin(std::string gdc_bin_file);
+  std::vector<std::shared_ptr<GdcBinBuf_ST>> gen_gdc_bin_stereo(int gdc_width, int gdc_height,int out_width, int out_height, int final_width, int final_height, 
+		std::vector<sensor_msgs::msg::CameraInfo> &cam_info, std::vector<sensor_msgs::msg::CameraInfo> &cal_cam_info,
+    double rotation = 0.0, double cal_rotate = 0.0, double cla_alpha = 0.0, bool pre_rotation = false);
+  std::shared_ptr<GdcBinBuf_ST> gen_gdc_bin(int gdc_width, int gdc_height,int out_width, int out_height,
+       sensor_msgs::msg::CameraInfo *cam_info, sensor_msgs::msg::CameraInfo *cal_cam_info,
+       double rotation = 0.0, double cal_rotate = 0.0, double cla_alpha = 0.0, bool pre_rotation = false);
+  std::shared_ptr<GdcBinBuf_ST> gen_gdc_bin_rotation(int gdc_width, int gdc_height,int out_width, int out_height, double rotation);
+  std::shared_ptr<GdcBinBuf_ST> gen_gdc_bin_json(std::string file);
+
+  virtual std::pair<double, double> calculatePinholeFOV(const cv::Mat& K_rect, int width, int hight);
+  virtual double computeInitAlpha(double target_hfov, double fov_min, double fov_max);
+  virtual double computeStereoAlphaFromFOV(double target_hfov, const cv::Mat& Kl, const cv::Mat& Dl,
+    const cv::Mat& Kr, const cv::Mat& Dr, const cv::Mat& R_rl, const cv::Mat& t_rl,
+    int in_gdc_width, int in_gdc_height, int out_gdc_width, int out_gdc_height, double& actual_hfov_l, double& actual_hfov_r);
+  virtual double find_best_fov_scale(const cv::Mat& Kl, const cv::Mat& Dl, const cv::Mat& Kr, const cv::Mat& Dr,
+      const cv::Mat& R_rl, const cv::Mat& t_rl, cv::Size in_size, cv::Size out_size);
+
+  virtual bool computeFisheyeStereoParamsFromFOV(
+      double target_hfov,
+      const cv::Mat &Kl, const cv::Mat &Dl,
+      const cv::Mat &Kr, const cv::Mat &Dr,
+      const cv::Mat &R_rl, const cv::Mat &t_rl,
+      cv::Size in_size, cv::Size out_size,
+      double &out_balance, double &out_fov_scale,
+      double &actual_hfov_l, double &actual_hfov_r,
+      double tol = 3.0,
+      int max_iter = 10);
+#endif
 };
 
 }  // namespace mipi_cam
